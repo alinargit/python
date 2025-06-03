2664b504436b351b407ba94c82bd837c1bd0b0ac
alinargit <127021881+alinargit@users.noreply.github.com>
2025-06-03 19:18:48 +0200

    y

---
+++ module_1/variables.py
@@ -0,0 +1,28 @@
+temperature = 20
+name = "John"
+
+print(temperature)
+print(name)
+
+print(type(temperature))
+print(type(name))
+
+# Declare two variables
+num1 = 10
+num2 = 5
+
+# Perform calculations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Update the value of a variable
+num1 = 15
+print("Updated num1:", num1)
+
+# Combine values (firstname and lastname)
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Full name:", full_name)
