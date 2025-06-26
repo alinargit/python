d3a8e7db37bcc9e5e7c16df7efc5dcc66de33604
alinargit <127021881+alinargit@users.noreply.github.com>
2025-06-26 19:20:12 +0200

    module_7

---
+++ module_7/explicitTypeCasting.py
@@ -0,0 +1,18 @@
+# Example 1
+
+age = 25
+
+age_as_str = str(age)
+
+print(age_as_str, "of type", type(age_as_str))
+
+# Example 2
+
+print(bool(0)) #Output: False
+print(bool(42)) #Output: True
+
+print(bool("")) #Output: False
+print(bool("Hello")) #Output: True
+
+print(bool([])) #Output: False
+print(bool(None)) #Output: False
---
+++ module_7/implicitTypeCasting.py
@@ -0,0 +1,20 @@
+# Example 1
+x = 32
+y = 5.3
+
+result = x + y
+print(result, "of type", type(result))
+
+# Example 2
+age = 25
+message = "I am " + str(age) + " years old"
+print(message)
+
+# Example 3
+a = 5
+b = "3"
+result1 = a * int(b)
+print(result1, "of type", type(result1))
+
+# Example 4
+#z = int("abc")
